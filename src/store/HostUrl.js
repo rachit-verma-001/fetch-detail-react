99bca86e49f2485ecf8f8568ad01500f50a0eb0a
Rachit Verma <rachit.verma@protonshub.in>
2021-12-24 11:55:18 +0530

    lets encrypt changes

--- src/store/HostUrl.js
+++ src/store/HostUrl.js
@@ -1,5 +1,10 @@
 export const localUrl = "http://localhost:4000"
 // export const ngrokUrl = "http://localhost:4000"
 
-export const ngrokUrl = "https://0699-182-77-7-232.ngrok.io"
-// export const ngrokUrl = "https://azure-test-vm-window.eastus.cloudapp.azure.com/"
+// export const ngrokUrl = "https://0699-182-77-7-232.ngrok.io"
+export const ngrokUrl = "https://azure-test-vm-window.eastus.cloudapp.azure.com/"
+// export const ngrokUrl = "http://localhost:4001/"
+//
+
+
+// export const ngrokUrl = "https://4149-20-124-179-137.ngrok.io/"
